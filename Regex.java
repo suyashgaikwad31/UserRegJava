bd7b0c3ba20f8968a6cf4be41fc70c7ecedf3038
unknown <suyashgaikwad31@gmail.com>
2021-03-18 12:43:25 +0530

    added Regex.java

--- Regex.java
+++ Regex.java
@@ -1,12 +1,12 @@
 import java.util.regex.*;
-
-public class Mobnumber {
+//Rule No 1
+public class Password {
    public static void main(String args[]) {
-      String mobnumber = "91 9689887638";
-      String regex = "^[1-9]{2}\\s{0,1}[0-9]{5}[0-9]{5}$";
+      String password = "jdhuihjn";
+      String regex = "[a-z]{8}";
       Pattern p = Pattern.compile(regex);
-      Matcher m = p.matcher(mobnumber);
-      System.out.println("The MobNumber is: " + mobnumber);
-      System.out.println("Is the MobNumber valid? " + mobnumber.matches(regex));
+      Matcher m = p.matcher(password);
+      System.out.println("The Password is: " + password);
+      System.out.println("Is the Password valid? " + password.matches(regex));
    }
 }
