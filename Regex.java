10b5be720fe66a4b9ce3286f6928451cb96d9df3
unknown <suyashgaikwad31@gmail.com>
2021-03-18 14:37:15 +0530

    added Regex.java

--- Regex.java
+++ Regex.java
@@ -1,9 +1,9 @@
 import java.util.regex.*;
-//Rule No 2
+//Rule No 3
 public class Password {
    public static void main(String args[]) {
-      String password = "jdFuiOjn";
-      String regex = "[a-z A-Z]{8}";
+      String password = "jdF5iOj8";
+      String regex = "[0-9 a-z A-Z]{8}";
       Pattern p = Pattern.compile(regex);
       Matcher m = p.matcher(password);
       System.out.println("The Password is: " + password);
