66eee5dea8adce26acb3e8a07bddd4cbd10b826d
unknown <suyashgaikwad31@gmail.com>
2021-03-18 12:08:46 +0530

    added Regex.java

--- Regex.java
+++ Regex.java
@@ -1,12 +1,12 @@
 import java.util.regex.*;
 
-public class FirstName {
+public class LastName {
    public static void main(String args[]) {
-      String firstname = "Suyash";
+      String lastname = "Gaikwad";
       String regex = "^[A-Z]{1}[a-z]{3,}$";
       Pattern p = Pattern.compile(regex);
-      Matcher m = p.matcher(firstname);
-      System.out.println("The Firstname is: " + firstname);
-      System.out.println("Is the Firstname valid? " + firstname.matches(regex));
+      Matcher m = p.matcher(lastname);
+      System.out.println("The Lastname is: " + lastname);
+      System.out.println("Is the Lastname valid? " + lastname.matches(regex));
    }
 }
